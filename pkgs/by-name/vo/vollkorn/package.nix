ad4f546c111a422fefa1bb320741d41050daf9bf
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-02 15:50:26 -0800

    vollkorn: use installFonts hook

--- pkgs/by-name/vo/vollkorn/package.nix
+++ pkgs/by-name/vo/vollkorn/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenvNoCC,
   fetchzip,
+  installFonts,
 }:
 
 stdenvNoCC.mkDerivation rec {
@@ -16,13 +17,13 @@ stdenvNoCC.mkDerivation rec {
     hash = "sha256-oG79GgCwCavbMFAPakza08IPmt13Gwujrkc/NKTai7g=";
   };
 
+  nativeBuildInputs = [ installFonts ];
+
   installPhase = ''
     runHook preInstall
 
     mkdir -pv $out/share/{doc/${pname}-${version},fonts/{opentype,truetype,WOFF,WOFF2}}
     cp -v {Fontlog,OFL-FAQ,OFL}.txt $out/share/doc/${pname}-${version}/
-    cp -v PS-OTF/*.otf $out/share/fonts/opentype
-    cp -v TTF/*.ttf $out/share/fonts/truetype
     cp -v WOFF/*.woff $out/share/fonts/WOFF
     cp -v WOFF2/*.woff2 $out/share/fonts/WOFF2
 

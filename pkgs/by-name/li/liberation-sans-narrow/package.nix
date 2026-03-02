4bc9dcfd98a75bf91962d62d881ad8ccac0988c6
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-01 23:00:38 -0800

    liberation-sans-narrow: cleanup

--- pkgs/by-name/li/liberation-sans-narrow/package.nix
+++ pkgs/by-name/li/liberation-sans-narrow/package.nix
@@ -1,49 +1,47 @@
 {
-  lib,
-  stdenv,
   fetchFromGitHub,
   fontforge,
-  python3Packages,
-  python3,
   installFonts,
+  lib,
+  stdenv,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "liberation-sans-narrow";
   version = "1.07.6";
 
   src = fetchFromGitHub {
     owner = "liberationfonts";
     repo = "liberation-sans-narrow";
-    rev = version;
-    sha256 = "1qw554jbdnqkg6pjjl4cqkgsalq3398kzvww2naw30vykcz752bm";
+    tag = finalAttrs.version;
+    hash = "sha256-dYlyPpt+g8GVFZzvP1EaA1Ol38SMUCmveRPbtiQpheM=";
   };
 
   nativeBuildInputs = [
     fontforge
     installFonts
-    python3Packages.fonttools
-    python3
   ];
 
   installPhase = ''
     runHook preInstall
-    install -m444 -Dt $out/doc/${pname}-${version} AUTHORS ChangeLog COPYING License.txt README.rst
+
+    install -Dm644 {AUTHORS,ChangeLog,COPYING,License.txt,README.rst} \
+      --target-directory=$out/doc/${finalAttrs.pname}-${finalAttrs.version}
+
     runHook postInstall
   '';
 
   meta = {
-    description = "Liberation Sans Narrow Font Family is a replacement for Arial Narrow";
+    description = "Replacement Font Family for Arial Narrow";
     longDescription = ''
       Liberation Sans Narrow is a font originally created by Ascender
-      Inc and licensed to Oracle Corporation under a GPLv2 license. It is
-      metrically compatible with the commonly used Arial Narrow fonts
-      on Microsoft systems. It is no longer distributed with the
-      latest versions of the Liberation Fonts, as Red Hat has changed the
-      license to the Open Font License.
+      Inc and licensed to Oracle Corporation under a GPLv2 license.
+      It is metrically compatible with the commonly used Arial Narrow
+      fonts on Microsoft systems.  It is no longer distributed with
+      the latest versions of the Liberation Fonts, as Red Hat has
+      changed the license to the Open Font License.
     '';
-
     license = lib.licenses.gpl2;
     homepage = "https://github.com/liberationfonts";
   };
-}
+})

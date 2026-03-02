10f98f59d4d59364436170987288ab0ea82167f2
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-01 22:19:42 -0800

    league-gothic: cleanup

--- pkgs/by-name/le/league-gothic/package.nix
+++ pkgs/by-name/le/league-gothic/package.nix
@@ -1,8 +1,8 @@
 {
-  lib,
   fetchzip,
-  stdenvNoCC,
   installFonts,
+  lib,
+  stdenvNoCC,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
@@ -17,18 +17,24 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ installFonts ];
 
   meta = {
-    description = "Revival of an old classic, Alternate Gothic #1";
+    description = "League Gothic";
     longDescription = ''
-      League Gothic is a revival of an old classic, and one of our favorite
-      typefaces, Alternate Gothic #1. It was originally designed by Morris
-      Fuller Benton for the American Type Founders Company in 1903. The company
-      went bankrupt in 1993, and since the original typeface was created before
-      1923, the typeface is in the public domain.
+      League Gothic is a revival of an old classic, and one of our
+      favorite typefaces, Alternate Gothic #1.  It was originally
+      designed by [Morris Fuller
+      Benton](https://en.wikipedia.org/wiki/Morris_Fuller_Benton) for
+      the [American Type
+      Founders](https://en.wikipedia.org/wiki/American_Type_Founders)
+      Company in 1903.  The company went bankrupt in 1993, and since
+      the original typeface was created over 95 years ago, the
+      typeface is in the public domain.
 
-      We decided to make our own version, and contribute it to the Open Source
-      Type Movement. Thanks to a commission from the fine & patient folks over
-      at WND.com, it’s been revised & updated with contributions from Micah
-      Rich, Tyler Finck, and Dannci, who contributed extra glyphs.
+      We decided to make our own version, and contribute it to the
+      Open Source Type Movement.  Thanks to a commission from the fine
+      & patient folks over at [WND.com](https://www.wnd.com), it’s
+      been revised & updated with contributions from Micah Rich, Tyler
+      Finck, and [Dannci](https://twitter.com/dannci), who contributed
+      extra glyphs.
     '';
     homepage = "https://www.theleagueofmoveabletype.com/league-gothic";
     license = lib.licenses.ofl;

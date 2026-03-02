b56356c780fa07e58872d18a2243fffbb61dffb6
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-02 15:57:05 -0800

    vollkorn: cleanup

--- pkgs/by-name/vo/vollkorn/package.nix
+++ pkgs/by-name/vo/vollkorn/package.nix
@@ -1,17 +1,17 @@
 {
-  lib,
-  stdenvNoCC,
   fetchzip,
   installFonts,
+  lib,
+  stdenvNoCC,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "vollkorn";
   version = "4.105";
 
   src = fetchzip {
     url = "http://vollkorn-typeface.com/download/vollkorn-${
-      builtins.replaceStrings [ "." ] [ "-" ] version
+      builtins.replaceStrings [ "." ] [ "-" ] finalAttrs.version
     }.zip";
     stripRoot = false;
     hash = "sha256-oG79GgCwCavbMFAPakza08IPmt13Gwujrkc/NKTai7g=";
@@ -22,19 +22,19 @@ stdenvNoCC.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -pv $out/share/{doc/${pname}-${version},fonts/{opentype,truetype,WOFF,WOFF2}}
-    cp -v {Fontlog,OFL-FAQ,OFL}.txt $out/share/doc/${pname}-${version}/
-    cp -v WOFF/*.woff $out/share/fonts/WOFF
-    cp -v WOFF2/*.woff2 $out/share/fonts/WOFF2
+    install -Dm644 WOFF/*.woff --target-directory=$out/share/fonts/WOFF
+    install -Dm644 WOFF2/*.woff2 --target-directory=$out/share/fonts/WOFF2
+    install -Dm644 {Fontlog,OFL-FAQ,OFL}.txt \
+      --target-directory=$out/share/doc/${finalAttrs.pname}-${finalAttrs.version}
 
     runHook postInstall
   '';
 
   meta = {
-    homepage = "http://vollkorn-typeface.com/";
     description = "Free and healthy typeface for bread and butter use";
+    homepage = "http://vollkorn-typeface.com";
     license = lib.licenses.ofl;
-    platforms = lib.platforms.all;
     maintainers = [ lib.maintainers.schmittlauch ];
+    platforms = lib.platforms.all;
   };
-}
+})

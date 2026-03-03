39af3fc6f6322681577f05be7ca49a9b7b24e721
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-03 12:56:27 -0800

    amiri: cleanup

--- pkgs/by-name/am/amiri/package.nix
+++ pkgs/by-name/am/amiri/package.nix
@@ -5,12 +5,12 @@
   installFonts,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "amiri";
   version = "1.003";
 
   src = fetchzip {
-    url = "https://github.com/aliftype/amiri/releases/download/${version}/Amiri-${version}.zip";
+    url = "https://github.com/aliftype/amiri/releases/download/${finalAttrs.version}/Amiri-${finalAttrs.version}.zip";
     hash = "sha256-BsYPMBlRdzlkvyleZIxGDuGjmqhDlEJ4udj8zoKUSzA=";
   };
 
@@ -19,17 +19,36 @@ stdenvNoCC.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/doc/${pname}-${version}
-    mv {*.html,*.txt,*.md} $out/share/doc/${pname}-${version}/
+    install -Dm644 {*.html,*.txt,*.md} \
+      --target-directory=$out/share/doc/${finalAttrs.pname}-${finalAttrs.version}
 
     runHook postInstall
   '';
 
   meta = {
     description = "Classical Arabic typeface in Naskh style";
-    homepage = "https://www.amirifont.org/";
+    longDescription = ''
+      Amiri (أميري) is a classical Arabic typeface in Naskh style for
+      typesetting books and other running text.
+
+      Amiri is a revival of the beautiful typeface pioneered in early
+      20th century by Bulaq Press in Cairo, also known as Amiria
+      Press, after which the font is named.
+
+      The uniqueness of this typeface comes from its superb balance
+      between the beauty of Naskh calligraphy on one hand, the
+      constraints and requirements of elegant typography on the other.
+      Also, it is one of the few metal typefaces that were used in
+      typesetting the Quran, making it a good source for a digital
+      typeface to be used in typesetting Quranic verses.
+
+      Amiri project aims at the revival of the aesthetics and
+      traditions of Arabic typesetting, and adapting it to the era of
+      digital typesetting, in a publicly available form.
+    '';
+    homepage = "https://www.amirifont.org";
     license = lib.licenses.ofl;
     maintainers = [ lib.maintainers.vbgl ];
     platforms = lib.platforms.all;
   };
-}
+})

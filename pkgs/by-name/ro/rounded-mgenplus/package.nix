f1f965822343ee8fceac5130489a60ee5afca3a9
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-02 17:03:14 -0800

    rounded-mgenplus: cleanup

--- pkgs/by-name/ro/rounded-mgenplus/package.nix
+++ pkgs/by-name/ro/rounded-mgenplus/package.nix
@@ -1,17 +1,17 @@
 {
-  lib,
-  stdenvNoCC,
   fetchurl,
   installFonts,
+  lib,
   p7zip,
+  stdenvNoCC,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "rounded-mgenplus";
   version = "20150602";
 
   src = fetchurl {
-    url = "https://osdn.jp/downloads/users/8/8598/${pname}-${version}.7z";
+    url = "https://osdn.jp/downloads/users/8/8598/${finalAttrs.pname}-${finalAttrs.version}.7z";
     hash = "sha256-7OpnZJc9k5NiOPHAbtJGMQvsMg9j81DCvbfo0f7uJcw=";
   };
 
@@ -24,9 +24,9 @@ stdenvNoCC.mkDerivation rec {
 
   meta = {
     description = "Japanese font based on Rounded M+ and Noto Sans Japanese";
-    homepage = "http://jikasei.me/font/rounded-mgenplus/";
+    homepage = "http://jikasei.me/font/rounded-mgenplus";
     license = lib.licenses.ofl;
-    platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ mnacamura ];
+    platforms = lib.platforms.all;
   };
-}
+})
